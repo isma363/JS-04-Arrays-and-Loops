3bd9b1254f076852ece1bc2462ec4e91e247d393
isma363 <95891457+isma363@users.noreply.github.com>
2022-01-18 22:09:18 -0600

    message

---
+++ script.js
@@ -0,0 +1,54 @@
+//Exercise #1
+//part 1
+// var personas =["Sofia", "David","Juan"];
+// console.log(personas);
+// //part 2
+// personas.push("Sara","Augustin");
+// personas.shift();
+// console.log(personas);
+// personas.splice(1,0,"Renata");
+// personas.push("Elena")
+// console.log(personas);
+
+//Exercise #2
+// var ciclo=["*"];
+// console.log(ciclo);
+// for (let contador=0;contador<=5;contador++){
+//     ciclo.push("*")
+//     console.log(ciclo);
+// }
+
+
+//Exercise #3
+
+//Parte 1
+// var xValue=10
+// while(xValue>0){
+//     xValue=xValue-.5
+//     console.log("xValue= "+xValue);
+// }
+
+//Parte 2
+// var contar=101
+// while(contar>1){
+//     contar=contar-1
+//     console.log(contar);
+// }
+
+//Parte 3
+// var n=6
+// var contador=0
+// while (contador < n) {
+//     contador = contador + 1;
+//     console.log("["+contador+"]");
+// }
+
+//Parte 4
+// var n=19
+// var contador=0
+// suma=0
+// while (contador < n) {
+//     contador=contador+1
+//     suma = suma+contador;
+//     console.log(suma)
+// }
